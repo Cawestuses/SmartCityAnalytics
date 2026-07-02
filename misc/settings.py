8bd7eadf92f2eee43ce176c000809b0267e89fe8
Cawestuses <markleonov2010@gmail.com>
2026-07-02 13:36:00 +0500

    feat: added settings.py for saving basic settings, such as main statistics

---
+++ misc/settings.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+STATS = ["Temperature(C)", "Wet(%)", "Air Pollution(PM2.5)", "Noise(dB)", "Car Flow(car/day)"
+         "Energy consumption(kWd)", "People quantity(people)", "Car crashes(crash/month)"]
+
